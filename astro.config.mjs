3c20eac66feb97eb3d1a7f2be5663417b5f6ce22
Murad Madi <muradmadi@gmail.com>
2026-01-24 18:38:51 +0100

    Initial commit, env setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
+import keystatic from '@keystatic/astro';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react(), sitemap(), keystatic()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
