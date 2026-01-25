952409d4385308e6fd38daae08f48398afdadbc4
Murad Madi <muradmadi@gmail.com>
2026-01-25 19:51:57 +0100

    Image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,17 @@ export default defineConfig({
     }
   },
 
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        limitInputPixels: false,
+      },
+    },
+    domains: [],
+    remotePatterns: [],
+  },
+
   output: 'static',
   integrations: [
     react(),
